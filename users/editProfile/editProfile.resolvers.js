ce5dbe19b4012a9135d026d45eff4ed406b0342f
smhj1003 <57647004+smhj1003@users.noreply.github.com>
2022-05-04 13:29:00 +0900

    add user.utils & header token

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -5,15 +5,17 @@ export default {
   Mutation: {
     editProfile: async (
       _,
-      { userName, phoneNumber, organization, email, password: newPassword }
+      { userName, phoneNumber, organization, email, password: newPassword },
+      { loggedInUser }
     ) => {
+      console.log(loggedInUser);
       let uglyPassword = null;
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
       }
       const updatedUser = await client.user.update({
         where: {
-          id: 1,
+          id: loggedInUser.id,
         },
         data: {
         userName,
