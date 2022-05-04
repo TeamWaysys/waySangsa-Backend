bb88b98c843157afef446f95c69faf2471cf33a9
smhj1003 <57647004+smhj1003@users.noreply.github.com>
2022-05-04 14:41:29 +0900

    fix

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
-import schema from "./schema";
+
 import { getUser } from "./users/users.utils";
+import { typeDefs, resolvers } from "./schema";
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
-  schema,
-  context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-    };
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+        return {
+        loggedInUser: await getUser(req.headers.token),
+        
+        };
   },
 });
 server
